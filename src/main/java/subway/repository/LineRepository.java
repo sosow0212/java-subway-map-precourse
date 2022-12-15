66dd8f89ce5990468b5d2045ed306a44d36bb311
sosow0212 <sosow0212@naver.com>
2022-12-15 16:04:06 +0900

    refactor : (Repository) 이미 존재하는지 확인하는 기능 구현

--- src/main/java/subway/repository/LineRepository.java
+++ src/main/java/subway/repository/LineRepository.java
@@ -1,10 +1,12 @@
 package subway.repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import subway.domain.Line;
+import subway.domain.Station;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
@@ -13,11 +15,20 @@ public class LineRepository {
         return Collections.unmodifiableList(lines);
     }
 
-    public static void addLine(Line line) {
-        lines.add(line);
+    public static void addLine(String lineName, Station upStation, Station downStation) {
+        lines.add(new Line(lineName, Arrays.asList(upStation, downStation)));
     }
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static boolean hasBeenLineNameAlready(String lineName) {
+        for (Line line : lines) {
+            if (line.getName().equals(lineName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/subway/repository/StationRepository.java
+++ src/main/java/subway/repository/StationRepository.java
@@ -1,9 +1,11 @@
 package subway.repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import subway.domain.Station;
 
 public class StationRepository {
@@ -17,7 +19,7 @@ public class StationRepository {
         stations.add(station);
     }
 
-    public static boolean hasStationName(String stationName) {
+    public static boolean hasBeenStationNameAlready(String stationName) {
         for (Station station : stations) {
             if (station.getName().equals(stationName)) {
                 return true;
@@ -28,5 +30,12 @@ public class StationRepository {
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    public static Station findByName(String name) {
+        return stations().stream()
+                .filter(i -> i.getName().equals(name))
+                .findAny()
+                .orElseGet(null);
     }
 }
