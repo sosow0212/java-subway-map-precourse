70888440f0670613be8af3a36a42093ebc3e02de
sosow0212 <sosow0212@naver.com>
2022-12-15 14:11:49 +0900

    feat : (Line) 생성자 및 필드 생성

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,14 +1,23 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+    private List<Station> stations;
 
-    public Line(String name) {
+    public Line(String name, List<Station> stations) {
+        this.stations = stations;
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public List<Station> getStations() {
+        return this.stations;
     }
 
     // 추가 기능 구현
